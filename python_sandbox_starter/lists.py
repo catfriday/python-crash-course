3fd9a4d208319b7e0a52a2870418f3b1f83b7950
Catrina Friday <cat.friday@gmail.com>
2021-01-11 17:27:49 -0500

    lists file

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,39 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+#  Create list
+numbers = [1,2, 3, 4]
+fruits = ['Apples', 'Oranges', 'Grapes']
+
+#  Use a constructor
+number2 = list((1, 2, 3, 4))
+
+# Get length below
+print(len(fruits))
+
+print(fruits[1])
+
+# Append to a list
+fruits.append("Mangos")
+
+# Remove form list
+fruits.remove("Grapes")
+
+#  Insert into list by position
+fruits.insert(2, 'Strawberries')
+
+# Change value
+fruits[0] = 'Blueberries'
+
+# Remove with pop
+fruits.pop(2)
+
+# Reverser the array 
+fruits.reverse()
+
+# Sort list
+fruits.sort()
+
+# Reverse Sort 
+fruits.sort(reverse=True)
+
+print(fruits)
