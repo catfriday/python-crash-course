5c15615c95b0abcc68d13626c3aa2e5ea7fdc381
Catrina Friday <cat.friday@gmail.com>
2021-01-12 16:21:31 -0500

    loops finished

--- python_sandbox_starter/loops.py
+++ python_sandbox_starter/loops.py
@@ -1,5 +1,35 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+people = ['John', 'Paul', 'Sarah', 'Susan']
 
+
+# simple for loop
+# for person in people:
+#     print(f'Current Person: {person}')
+
+# Break
+# for person in people:
+#     if person == 'Sarah':
+#         break 
+#     print(f'Current Person: {person}')
+
+# continue prints everything except Sarah
+# for person in people:
+#     if person == 'Sarah':
+#         continue
+#     print(f'Current Person: {person}')
+
+
+# range
+# for i in range(len(people)):
+#     print(people[i])
+
+# for i in range(0,11):
+#     print(f'Number: {i}')
 
 # While loops execute a set of statements as long as a condition is true.
+
+# count = 0
+# while count <= 10:
+#     print(count)
+#     count += 1
